62c1d30dddb0d9a6fcb45f826c0c69014a3ec66a
Jola-Witaszak <jola-w@o2.pl>
2021-01-31 22:33:59 +0100

    task-19.2 "database connection"

--- src/main/java/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -2,11 +2,22 @@ package com.crud.tasks.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Entity(name = "tasks")
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class Task {
+    @Id
+    @GeneratedValue
     private Long id;
+
+    @Column(name = "name")
     private String title;
+
+    @Column(name = "description")
     private  String content;
 }
