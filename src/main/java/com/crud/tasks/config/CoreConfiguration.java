7c02944dd704df472bb9b70b8bec4c6c8ac6b292
Jola-Witaszak <jola-w@o2.pl>
2021-03-31 20:35:17 +0200

    Task-23.2 "fix after review part 2"

--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -22,7 +22,7 @@ public class CoreConfiguration {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("hidden-lowlands-06521.herokuapp.com/v1"))
+                .apis(RequestHandlerSelectors.basePackage("com.crud.tasks.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
