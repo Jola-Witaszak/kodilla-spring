187046cc35d2d3ccb1f46530187d23f3a3211352
Jola-Witaszak <jola-w@o2.pl>
2021-04-05 14:17:44 +0200

    Task - 23.4 "emailScheduler"

--- src/main/java/com/crud/tasks/domain/Mail.java
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -3,7 +3,6 @@ package com.crud.tasks.domain;
 import lombok.Builder;
 import lombok.Getter;
 
-import java.time.Instant;
 import java.util.Date;
 
 @Builder
@@ -13,7 +12,7 @@ public class Mail {
     private final String replyTo;
     private final String mailTo;
     private final String toCc;
-    private final Date sentDate = Date.from(Instant.now());
+    private final Date sentDate;
     private final String subject;
     private final String message;
 }
--- src/main/java/com/crud/tasks/domain/Trello.java
+++ src/main/java/com/crud/tasks/domain/Trello.java
@@ -8,7 +8,7 @@ import lombok.Data;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Trello {
     @JsonProperty("board")
-    private String board;
+    private int board;
     @JsonProperty("card")
-    private String card;
+    private int card;
 }
