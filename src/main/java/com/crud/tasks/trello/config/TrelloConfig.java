b29acf491c75c1966f78412499b1846883e760ea
Jola-Witaszak <jola-w@o2.pl>
2021-04-29 15:40:31 +0200

    "Last commit"

--- src/main/java/com/crud/tasks/trello/config/TrelloConfig.java
+++ src/main/java/com/crud/tasks/trello/config/TrelloConfig.java
@@ -2,10 +2,12 @@ package com.crud.tasks.trello.config;
 
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @Getter
+@ConfigurationProperties(prefix = "trello")
 public class TrelloConfig {
 
     @Value("${trello.api.endpoint.prod}")
--- src/test/java/com/crud/tasks/trello/facade/TrelloFacadeTest.java
+++ src/test/java/com/crud/tasks/trello/facade/TrelloFacadeTest.java
@@ -12,8 +12,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.*;
 
