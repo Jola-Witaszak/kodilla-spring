a3b50ba4ac47a5334884f7f5c7056714a9a3c257
Jola-Witaszak <jola-w@o2.pl>
2021-05-02 16:58:36 +0200

    "remove NonNull from TrelloCardDto"

--- src/main/java/com/crud/tasks/domain/TrelloCardDto.java
+++ src/main/java/com/crud/tasks/domain/TrelloCardDto.java
@@ -1,20 +1,14 @@
 package com.crud.tasks.domain;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 
 @Data
 @NoArgsConstructor
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class TrelloCardDto {
-    @NonNull
+
     private String name;
-    @NonNull
     private String description;
-    @NonNull
     private String pos;
-    @NonNull
     private String idList;
 }
