45c5eb7ba23f75337131ea1912b3203416739fde
Jola-Witaszak <jola-w@o2.pl>
2021-05-03 07:35:17 +0200

    "up-1 03.05.21"

---
+++ src/main/java/com/crud/tasks/controller/StaticWebPageController.java
@@ -0,0 +1,24 @@
+package com.crud.tasks.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Controller
+@CrossOrigin("*")
+public class StaticWebPageController {
+
+    @RequestMapping("/")
+    public String index(Map<String, Object> model) {
+        model.put("variable", "My Thymeleaf variable");
+        model.put("one", 1);
+        model.put("two", 2);
+        model.put("mul", "2 * 2 = ");
+        model.put("plus", "2 * 2  + 2 = ");
+        model.put("min", "2 - 2 * 2 = ");
+
+        return "index";
+    }
+}
