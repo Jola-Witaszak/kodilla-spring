dfd81dbadc41e81a5645895424487ec98e3d5409
Jola-Witaszak <jola-w@o2.pl>
2021-05-08 15:01:08 +0200

    Task-32.3 create an email template sent once a day

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class MailCreatorService {
 
@@ -18,6 +21,12 @@ public class MailCreatorService {
     private TemplateEngine templateEngine;
 
     public String buildTrelloCardEmail(String message) {
+
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your tasks");
+        functionality.add("Provides connection with Trello Account");
+        functionality.add("Application allows sending tasks to Trello");
+
         Context context = new Context();
         context.setVariable("message", message);
         context.setVariable("tasks_url", "http://localhost:8888/tasks_frontend");
@@ -26,6 +35,22 @@ public class MailCreatorService {
         context.setVariable("goodbye_message", adminConfig.getGoodByeMessage());
         context.setVariable("company_details", adminConfig.getCompanyName());
 
+        context.setVariable("show_button", true);
+        context.setVariable("is_friend", true);
+
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("application_functionality", functionality);
+
         return templateEngine.process("mail/created-trello-card-mail", context);
+    }
+
+    public String buildOnceInADayEmail(String message) {
+        Context context = new Context();
+        context.setVariable("message", message);
+        context.setVariable("tasks_url", "http://localhost:8888/tasks_frontend");
+        context.setVariable("button", "Visit website");
+        context.setVariable("admin_config", adminConfig);
+
+        return templateEngine.process("mail/once-in-a-day-email", context);
     }
 }
